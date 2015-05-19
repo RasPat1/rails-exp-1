6973ffe9fb7eba92ee67e1b4331ba5ae93c7ac2c
Rasesh Patel <Rasesh@shoptiques.com>
2015-05-18 20:32:11 -0400

    duplicate everything weirdly to make a point

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Vamos a la playa, ohohohoh"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Vamos a la playa, ohohohoh"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Vamos a la playa, ohohohoh"
   end
 
 end
