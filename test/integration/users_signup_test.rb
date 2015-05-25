dd491a3891002fc5f43beb7f95de95d83059df04
Rasesh Patel <Rasesh@shoptiques.com>
2015-05-25 13:19:04 -0400

    valid user signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "user sign ups" do
+  test "invalid user sign" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, user: { name: "",
@@ -13,5 +13,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
+  test "valid user sign up" do
+    assert_difference 'User.count' do
+      post users_path, user: { name: "Slumdog MM",
+                               email: "latika@shaadi.com",
+                               password: "Amitabh11",
+                               password_confirmation: "Amitabh11"}
+    end
+    assert_template 'users/show'
+  end
 
 end
